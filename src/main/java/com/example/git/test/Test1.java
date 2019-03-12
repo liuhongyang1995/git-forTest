78e5e67cc31a66e922f4518bd7e3238f6d3fdc46
liuhongyang <keqqq007@vip.qq.com>
2019-03-12 15:30:26 +0800

    提交test

--- src/main/java/com/example/git/test/Test1.java
+++ src/main/java/com/example/git/test/Test1.java
@@ -7,6 +7,6 @@ public class Test1 {
     public static void main(String[] args) {
     System.out.println("dev-100");
     System.out.println("master");
-
+        System.out.println("hhhh");
     }
 }
